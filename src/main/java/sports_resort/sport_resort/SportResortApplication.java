bf11d0a2deb23d9d98cf71b9fc7791c50001bbfe
neyzar <lucas.narguet451@gmail.com>
2025-06-02 11:43:33 +0200

    init

---
+++ src/main/java/sports_resort/sport_resort/SportResortApplication.java
@@ -0,0 +1,13 @@
+package sports_resort.sport_resort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportResortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportResortApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sports_resort/sport_resort/SportResortApplicationTests.java
@@ -0,0 +1,13 @@
+package sports_resort.sport_resort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportResortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
