87cab0e60bd37f55922f498c7a5d1fb5d3797d92
mateo <grange.mateo45@gmail.com>
2025-06-03 17:06:25 +0200

    feat front(hero section homepage)

--- front-end/sports-resort/app/components/styles/Button.tsx
+++ front-end/sports-resort/app/components/styles/Button.tsx
@@ -1,7 +1,17 @@
-export default function Button() {
+import {cn} from "~/lib/utils";
+
+type Props = {
+    name:string;
+    className?:string;
+    href:string;
+}
+
+export default function Button({name, className, href}: Props) {
     return (
         <>
-            button
+            <a href={href} className={cn('bg-blue border-blue hover:text-blue text-white border-2 rounded-full px-6 py-2 w-fit hover:bg-transparent duration-200 ease-in-out uppercase font-bold m-1', className)}>
+                {name}
+            </a>
         </>
     )
 }
--- front-end/sports-resort/app/routes/accueil.tsx
+++ front-end/sports-resort/app/routes/accueil.tsx
@@ -1,11 +1,38 @@
 import Footer from "~/components/layout/Footer";
 import Header from "~/components/layout/Header";
+import Button from "~/components/styles/Button"
 
 export default function Accueil() {
   return(
       <>
           <Header />
 
+          <div className="min-h-[520px] w-full relative bg-cover" style={{
+              backgroundImage:
+                  "url(/medias/images/hero.jpg)",
+          }}>
+              <div className="absolute h-full w-full bg-[#000000a4]">
+                  <h1 className="text-heading1 font-bold text-center"> Réserve ta séance en quelques clics</h1>
+                  <p className="text-small">
+                      Que tu sois amateur de sensations fortes ou en quête de bien-être, notre plateforme te permet
+                      de réserver facilement tes séances dans les meilleures installations sportives autour de toi.
+                      Trouve ton sport, ton coach ou ton équipe, et reste en mouvement à ton rythme&nbsp;!
+                  </p>
+                  <Button name="Trouve ta séance idéale"
+                          className=""
+                          href="/"/>
+              </div>
+          </div>
+
+            <Button name="connexion"
+                    className="bg-red border-red hover:text-red"
+                    href="/"/>
+            <Button name="connexion"
+                    className=""
+                    href="/"/>
+            <Button name="inscription"
+                    className=""
+                    href="/"/>
           <Footer/>
       </>
   );
