ca2b4ccfeca1ee070ebeab4e4f3e7b3bdf5fb00d
pablitoodt <pablodeteba@icloud.com>
2025-06-03 15:55:32 +0200

    ajout formulaire connexion et inscription

--- front-end/sports-resort/app/routes/login.tsx
+++ front-end/sports-resort/app/routes/login.tsx
@@ -1,11 +1,15 @@
 import {Link} from "react-router";
 import Button from "~/components/styles/Button";
+import LoginForm from "~/components/styles/LoginForm";
+import RegisterForm from "~/components/styles/RegisterForm";
 
 export default function Login() {
-    return (
-     <>
-         <Link to="/">Accueil</Link>
-         <Button />
-     </>
-    );
+  return (
+    <>
+      <Link to="/">Accueil</Link>
+      <Button />
+      <LoginForm />
+      <RegisterForm />
+    </>
+  );
 }
