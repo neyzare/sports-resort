49d48a7902cc61b8bdd5b104a954114e0c21dcf5
mateo <grange.mateo45@gmail.com>
2025-06-05 23:52:02 +0200

    feat: front(fix profile page)

--- front-end/sports-resort/app/routes/profile.tsx
+++ front-end/sports-resort/app/routes/profile.tsx
@@ -1,5 +1,6 @@
 import Footer from "~/components/layout/Footer";
 import Header from "~/components/layout/Header";
+import Button from "~/components/styles/Button";
 
 export default function Sport () {
   return (
