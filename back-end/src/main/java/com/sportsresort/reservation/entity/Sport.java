045c35958a7757d9824747122cac351f9eec9324
neyzar <lucas.narguet451@gmail.com>
2025-06-10 16:05:25 +0200

    ajout logique sport

---
+++ back-end/src/main/java/com/sportsresort/reservation/entity/Sport.java
@@ -0,0 +1,26 @@
+package com.sportsresort.reservation.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+  @Getter @Setter @Builder
+  @NoArgsConstructor @AllArgsConstructor
+public class Sport {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private String description;
+  private String imageUrl;
+  private String lien;
+  private String emojie;
+}
