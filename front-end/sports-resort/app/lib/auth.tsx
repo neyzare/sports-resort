6bc935981195daaeb322685a9c20a3e6a6e7be00
grang <grange.mateo45@gmail.com>
2025-06-11 14:00:14 +0200

    feat front(add admin private page)

--- front-end/sports-resort/app/lib/auth.tsx
+++ front-end/sports-resort/app/lib/auth.tsx
@@ -1,3 +1,17 @@
 export const isAuthenticated = () => {
   return !!localStorage.getItem('jwt');
 };
+
+export const getUserRoleFromToken = (): string | null => {
+  const token = localStorage.getItem('jwt');
+  if (!token) return null;
+
+  try {
+    const base64Payload = token.split('.')[1];
+    const payload = JSON.parse(atob(base64Payload));
+    return payload.roles || null;
+  } catch (err) {
+    console.error('Erreur de décodage du JWT :', err);
+    return null;
+  }
+};
