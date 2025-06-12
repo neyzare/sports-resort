49cee9b13e87909d528f77193a7889d80be86ce5
pablitoodt <pablodeteba@icloud.com>
2025-06-12 22:04:32 +0200

    back user profil

---
+++ back-end/src/main/java/com/sportsresort/reservation/controller/UserController.java
@@ -0,0 +1,52 @@
+package com.sportsresort.reservation.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sportsresort.reservation.entity.User;
+import com.sportsresort.reservation.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+    
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @GetMapping("/{email}")
+    public Optional<User> getOneUser(@PathVariable String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @PutMapping("/users/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User updated) {
+        User user = userRepository.findById(id).orElseThrow();
+        user.setFirstname(updated.getFirstname());
+        user.setLastname(updated.getLastname());
+        user.setEmail(updated.getEmail());
+        user.setAddress(updated.getAddress());
+        
+        if (updated.getPassword() != null && !updated.getPassword().isBlank()) {
+            user.setPassword(passwordEncoder.encode(updated.getPassword()));
+        }
+
+        return userRepository.save(user);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
+    }
+}
